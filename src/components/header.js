48e025cb6eed3b9546caf99fa9b68a1175cea88a
omlinson <omlinson@PC194.Altitude.local>
2019-01-27 22:40:02 -0500

    my git is rusty

---
+++ src/components/header.js
@@ -0,0 +1,39 @@
+import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const Header = ({ siteTitle }) => (
+  <div  >
+    <div
+      style={{
+        backgroundColor:`#222222`, 
+        text:`white`,
+        link:`#b2a536`,
+        vlink:`#b2a536`,
+        alink:`#b2a536`,
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+  </div>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
---
+++ src/components/layout.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
+
+import Header from './header'
+import './layout.css'
+
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Header siteTitle="Navigation Element" />
+        <div
+          style={{
+            backgroundColor:`#222222`, 
+            text:`white`,
+            link:`#b2a536`,
+            vlink:`#b2a536`,
+            alink:`#b2a536`,
+          }}
+        >
+          {children}
+          <footer>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
+        </div>
+      </>
+    )}
+  />
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
